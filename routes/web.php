66bc87f2ba6569daad633ef3b18f459c7a795b77
Maarten de Jager <maartendejager@gmail.com>
2017-11-12 13:29:34 +0000

    Sets up the main structure (Model, Migration, Controller, Routes), for User, Group, Scan, Instantie, Question, Answer, and also Postcode

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,10 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('group', 'GroupsController');
+Route::resource('scan', 'ScansController');
+Route::resource('instantie', 'InstantiesController');
+Route::resource('question', 'QuestionsController');
+Route::resource('answer', 'AnswersController');
+Route::resource('postcode', 'PostcodesController');
