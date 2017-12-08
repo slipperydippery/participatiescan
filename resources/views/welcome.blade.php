fabda838a921590feaaef44dec55c841945f44f5
Maarten de Jager <maartendejager@gmail.com>
2017-12-08 13:43:37 +0100

    updates welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <section class="content--abovethefold">
             <div class="row container">
                 <h1 class="title"><span class="title--participatie">Participatie</span><span class="title--dot">.</span><span class="title--scan">scan</span></h1>
-                <h2 class="subtitle">Zet jouw kennis in. Verbeter de participatie van jongeren in een kwetsbare positie.</h2>
+                <h2 class="subtitle">In hoeverre voldoet jouw organisatie aan 15 belangrijke succesfactoren? Verbeter de participatie van jongeren in een kwetsbare positie.</h2>
                 <div class="featuredimg--container">
                     <div class="mainbutton--container">
                         <a href=" {{ route('startscan') }} " class="btn mainbutton">Doe de scan</a>
@@ -35,45 +35,6 @@
                         <img src="/img/nederland.svg" alt="">
                     </div>
                 </div>
-            </div>
-        </section>
-
-        <section class="content--fullwidthback">
-            <div class="row container">
-                <div class="col-sm-4">
-                    <div class="infoblock">
-                        <img src="/img/scoreboard.svg" alt="">
-                        <span class="highlight">Waar staat jouw organisatie ten aanzien van 15 belangrijke succesfactoren?</span>
-                    </div>
-                </div>
-                <div class="col-sm-4">
-                    <div class="infoblock">
-                        <img src="/img/scales.svg" alt="">
-                        <span class="highlight">Waar staan andere organisaties ten aanzien van 15 belangrijke succesfactoren?</span>
-                    </div>
-                </div>
-                <div class="col-sm-4">
-                    <div class="infoblock">
-                        <img src="/img/regionh.svg" alt="">
-                        <span class="highlight">Ga aan tafel met partners uit jouw regio.</span>
-                    </div>
-                </div>
-            </div>
-        </section>
-    
-        <section>
-            <div class="row container">
-                <div class="col-sm-12">
-                    
-                    <h3 class="title--section">In hoeverre voldoet jouw organisatie aan belangrijke succesfactoren?</h3>
-                </div>
-            </div>
-            <div class="row container">
-                @foreach($scanmodel->themes as $theme)
-                    <div class="col-sm-4"> 
-                        <a href="#" class="btn themebutton themecolor-{{ $theme->id }}"> {{ $theme->title }} </a>
-                    </div>
-                @endforeach
             </div>
         </section>
     </div>
