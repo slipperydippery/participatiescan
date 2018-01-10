e9bb54ddb7dc4995020b2710f896d4dbe480793a
Maarten de Jager <maartendejager@gmail.com>
2018-01-10 11:00:05 +0000

    adds links to cijfers uit de regio and youtube videos

--- resources/views/pages/instructiefilms.blade.php
+++ resources/views/pages/instructiefilms.blade.php
@@ -17,25 +17,29 @@
         <div class="row content--page">
             <div class="col-sm-12 ">
                 <h4>Introductievideo</h4>
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/oOSoBnCPHlk" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/oOSoBnCPHlk" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe> <br>
+                <a href="https://www.youtube.com/watch?v=oOSoBnCPHlk">bekijk de video op youtube</a>
             </div>
         </div>
         <div class="row content--page">
             <div class="col-sm-12 ">
                 <h4>Thema 1: Focus op werk en talent</h4>
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/UjItL3RGbU0" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/UjItL3RGbU0" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe> <br>
+                <a href="https://www.youtube.com/watch?v=UjItL3RGbU0">bekijk de video op youtube</a>
             </div>
         </div>
         <div class="row content--page">
             <div class="col-sm-12 ">
                 <h4>Thema 2: Kwaliteit begeleiding</h4>
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/BCd3b2NfnDM" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/BCd3b2NfnDM" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe> <br>
+                <a href="https://www.youtube.com/watch?v=BCd3b2NfnDM">bekijk de video op youtube</a>
             </div>
         </div>
         <div class="row content--page">
             <div class="col-sm-12 ">
                 <h4>Thema 3: Effectieve samenwerking</h4>
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/tYNpt95zTWc" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/tYNpt95zTWc" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe> <br>
+                <a href="https://www.youtube.com/watch?v=tYNpt95zTWc">bekijk de video op youtube</a>
             </div>
         </div>
     </div>
--- resources/views/pages/pmonitor.blade.php
+++ resources/views/pages/pmonitor.blade.php
@@ -11,7 +11,7 @@
         <div class="row">
             <div class="col-md-12">
                 <h2 class="page--title"> Participatiemonitor </h2>
-                <span class="page--clarification">De cijfers die hier worden weergegeven geven aan welke factoren in de geschiedenis van een jongere hem of haar extra kwetsbaar maken om zonder werk en school te raken. Deze data en kennis over effectieve arbeidstoeleiding van de doelgroep leveren input voor een gesprek waarin kennis, reflectie en actie op tactisch niveau gecombineerd worden. Klik op de "i" voor een uitgebreide toelichting </span>
+                <span class="page--clarification">De cijfers die hier worden weergegeven geven aan welke factoren in de geschiedenis van een jongere hem of haar extra kwetsbaar maken om zonder werk en school te raken. Deze data en kennis over effectieve arbeidstoeleiding van de doelgroep leveren input voor een gesprek waarin kennis, reflectie en actie op tactisch niveau gecombineerd worden. Klik op de "i" voor een uitgebreide toelichting. Als er geen kaart verschijnt, volg dan <a href="https://monitoraoj.nl/arbeidsregio1" target="_blank">deze link</a>. </span>
 
             </div>
         </div>
