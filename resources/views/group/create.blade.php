e3a9dad2513f6f8b98032f86c4c231a625ed4260
Maarten de Jager <maartendejager@gmail.com>
2018-03-15 14:05:42 +0100

    adds text to create group.

--- resources/views/group/create.blade.php
+++ resources/views/group/create.blade.php
@@ -5,6 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
         	<h1>Maak een nieuwe groep aan:</h1>
+            <p>Wil je liever bij een groep aansluiten? <a href=" {{ URL::route('scan.create') }} ">Maak dan een scan aan</a>, en selecteer de groep waar je je bij aan wilt sluiten.</p>
 
         	<create-group
         		:instanties=" {{ $instanties }} "
